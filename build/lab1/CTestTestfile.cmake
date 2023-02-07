28bc5900066a59c41fb6fcfd145299d65b11b4c5
GerardoGarduno <gardunogerardo1@gmail.com>
2023-02-06 23:56:22 -0800

    turtle does circle

--- build/lab1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admin/Desktop/CSE180/src/lab1
-# Build directory: /home/admin/Desktop/CSE180/build/lab1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
